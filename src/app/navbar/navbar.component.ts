3d095995578bbaecc3483d050abfdce8db332daf
HafidElmoudden <abdulhafid858@gmail.com>
2025-05-18 18:05:16 +0000

    Add navigation component

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { AuthService } from '../services/auth.service';
+import { User } from '../model/user.model';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatDividerModule } from '@angular/material/divider';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css'],
+  standalone: true,
+  imports: [
+    RouterLink, 
+    RouterLinkActive,
+    CommonModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatMenuModule,
+    MatDividerModule
+  ]
+})
+export class NavbarComponent implements OnInit {
+  currentUser: User | null = null;
+
+  constructor(private authService: AuthService) { }
+
+  ngOnInit(): void {
+    this.authService.currentUser$.subscribe(user => {
+      this.currentUser = user;
+    });
+  }
+
+  logout(): void {
+    this.authService.logout();
+  }
+
+  isAdmin(): boolean {
+    return this.authService.hasRole('ADMIN');
+  }
+}
